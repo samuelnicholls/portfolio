b63534ba88197f49943b395466726bf2db3c4877
Samuel Nicholls <samuel.nicholls@hey.com>
2023-06-21 07:17:12 +0100

    Initial start on portfolio

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# NextJS Starter
+# Porfolio
 
-## About The Project
-
-This is the starter files for a NextJS project with TypeScript.
-
-## Built With
-- React
-- NextJS
-- Typescript
-- React Query
-- Tailwind CSS
-- ESLint
-- Prettier
+## TODO
---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="w-full bg-gray-800 border-t border-gray-200">
+      <div className="w-full mx-auto container md:p-6 p-4 flex">
+        <span className="text-sm text-gray-300 sm:text-center">
+          © {currentYear + ' '}
+          <a
+            href="https://samuelnicholls.dev/"
+            aria-label="Link to portfolio"
+            target="_blank"
+            className="hover:underline"
+          >
+            Samuel Nicholls
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
